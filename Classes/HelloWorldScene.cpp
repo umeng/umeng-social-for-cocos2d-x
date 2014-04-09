44a5fae9d3d071da86c91168a30fa0d74bedc594
hehonghui <hehonghui@umeng.com>
2014-04-09 15:15:09 +0800

    add comments

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -145,12 +145,12 @@ bool HelloWorld::init()
     // ********************  设置友盟的app key      ***********************************
     CCUMSocialSDK *sdk = CCUMSocialSDK::create();
     sdk->setAppKey("507fcab25270157b37000010");
-    sdk->setQQAppId("507fcab25270157b37000010");
-    sdk->setQZoneAppId("507fcab25270157b37000010");
-    sdk->setWeiXinAppId("507fcab25270157b37000010");
-    sdk->setYiXinAppKey("507fcab25270157b37000010");
-    sdk->setLaiwangAppId("507fcab25270157b37000010");
-    sdk->setLaiwangAppKey("507fcab25270157b37000010");
+    // sdk->setQQAppId("设置QQ的app id");
+    // sdk->setQZoneAppId("设置QQ空间的app id");
+    // sdk->setWeiXinAppId("设置微信和朋友圈的app id");
+    // sdk->setYiXinAppKey("设置易信和易信朋友圈的app id");
+    // sdk->setLaiwangAppId("设置来往和来往动态的app id");
+    // sdk->setLaiwangAppKey("设置来往和来往动态的app key");
     // 设置用户点击一条图文分享时用户跳转到的目标页面, 一般为app主页或者下载页面
     sdk->setTargetUrl("http://www.umeng.com/social");
     // ********************************************************************************
