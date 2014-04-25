84bf5f7eeed799a045a5974c71d3cef6fc57c55a
yeahugo <yejiahuan@umeng.com>
2014-04-25 13:07:37 +0800

    修改各个平台模块的开关

--- Classes/UmengSocial/Ios/UmSocialControllerIOS.h
+++ Classes/UmengSocial/Ios/UmSocialControllerIOS.h
@@ -17,7 +17,7 @@
 #define CC_ShareToQQOrQzone    1
 #define CC_ShareToLaiWang      1
 #define CC_ShareToYiXin        1
-#define CC_ShareToFacebook     1
+#define CC_ShareToFacebook     0
 #define CC_ShareToTwitter      1
 #define CC_ShareToInstagram    1
 
@@ -37,40 +37,26 @@ public:
     static void openShareWithImagePath(vector<int>* platform, const char* text, const char* imagePath,ShareEventHandler callback);
     static void directShare(const char* text, const char* imagePath, int platform, ShareEventHandler callback);
     
-#ifdef CC_ShareToQQOrQzone
     //设置QQ互联appid，appkey
     static void setQQAppIdAndAppKey(const char *appId,const char *appKey);
-#endif
-    
-#ifdef CC_ShareToWechat
+
     //设置微信appid
     static void setWechatAppId(const char *appId);
-#endif
-    
-#ifdef CC_ShareToLaiWang
+
     //设置来往appid，appkey
     static void setLaiwangAppInfo(const char *appId, const char *appKey, const char * appName);
-#endif
-    
-#ifdef CC_ShareToYiXin
+
     //设置易信appid
     static void setYiXinAppKey(const char *appKey);
-#endif
     
-#ifdef CC_ShareToFacebook
     //设置Facebook的appid
     static void setFacebookAppId(const char *appId);
-#endif
-    
-#ifdef CC_ShareToTwitter
+
     //打开Twitter的开关
     static void openTwitter();
-#endif
-    
-#ifdef CC_ShareToInstagram
+
     //打开Instagram的开关
     static void openInstagram();
-#endif
     
 private:
     static string m_appKey;
