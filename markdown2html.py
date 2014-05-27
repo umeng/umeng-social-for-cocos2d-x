964c4135207d85b078d39ef11247bdb42494de0e
hehonghui <hehonghui@umeng.com>
2014-05-27 16:25:27 +0800

    update md script and doc

--- markdown2html.py
+++ markdown2html.py
@@ -11,29 +11,17 @@
 
 import os, sys
 import markdown
-import codecs
-
-# hacked from python-markdown libary,
-# since it doesn't support preceeding 
-# by space, suck!
-from fenced_code import FencedCodeExtension
-
-# python encoding, really suck
-ENCODING = 'utf-8'
-CH_ENCODING = 'utf-8'
-
-reload(sys)
-sys.setdefaultencoding(ENCODING)
 
 # css used to render transfered html
 CSS = '<link rel="stylesheet" type="text/css" href="markdown.css"></link>'
-htmlHead = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>'+CSS+'</head><body>'
+htmlHead = '<html><head><meta http-equiv="Content-Type" content="text/html;     charset=utf-8"/>'+CSS+'</head><body>'
 htmlEnd = '</body></html>'
+# python encoding, really suck
+ENCODING = 'utf-8'
+
 # githup flavored markdown extentions, 
 # used to render github styled syntex blocks
-EXTENTIONS = [FencedCodeExtension(), 'tables']
-
-markdown.Extension.config = {}
+EXTENTIONS = ['fenced_code', 'tables']
 
 # check existence of css script.
 # the script will not excute till css is put 
@@ -41,6 +29,7 @@ markdown.Extension.config = {}
 def checkcss(md_name):
     path, md_name = os.path.split(md_name)
     css = os.path.join(path, 'markdown.css')
+    print css
     return os.path.isfile(css)
 
 # transfer markdown to html.
@@ -51,7 +40,6 @@ def md2html(md_name):
     md_text = unicode(md_text, ENCODING)
     md_file.close()
 
-    #md_html = markdown2.markdown(md_text, extras=EXTENTIONS)
     md_html = markdown.markdown(md_text, EXTENTIONS) 
     md_html = md_html.encode(ENCODING).strip()
     return md_html
@@ -60,40 +48,18 @@ def md2html(md_name):
 def save2html(md_name, md_html):
     path, md_name = os.path.split(md_name)
     html_name = os.path.join(path, md_name.replace('md', 'html'))
-    html_file = codecs.open(html_name, 'w', CH_ENCODING)
+    html_file = open(html_name, 'w')
     html_file.write(htmlHead + os.linesep + htmlEnd)
     html_file.write(md_html)
     html_file.close()
 
-# given a markdown file, save it to html file
-def processone(md_name):
-    if not (os.path.isfile(md_name) and md_name.endswith('.md')):
-        print 'not a markdown file'
+if __name__ == '__main__':
+    md_name = sys.argv[1]
+    if not checkcss(md_name):
+        print 'no css file!'
+        sys.exit()
 
     md_content = md2html(md_name)
     save2html(md_name, md_content)
+    
 
-# give a directory, save all the markdown files
-# to html files
-def processmulti(md_dir):
-    for f in os.listdir(md_dir):
-        if f.endswith('.md'):
-            md = os.path.join(md_dir, f)
-            processone(md)
-            print 'processed %s' % md
-
-if __name__ == '__main__':
-    md_name = sys.argv[1]
-    md_css = None
-    if len(sys.argv)> 2:
-        md_css = sys.argv[2]
-
-    if os.path.isfile(md_name):
-        if md_css == None and not checkcss(md_name):
-            print 'no css file!'
-            sys.exit() 
-        processone(md_name)
-    elif os.path.isdir(md_name):
-        processmulti(md_name)
-    else:
-        print 'can not recognize file or directory'
